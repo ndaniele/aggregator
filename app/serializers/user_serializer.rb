e2bbbd518601599aae1460cb472572381438aa35
ndaniele <n.daniele@gmail.com>
2016-11-25 16:19:32 -0500

    added groups to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email 
   has_many :answers 
   has_many :questions
-    has_many :memberships, serializer: UserMembershipsSerializer
+  has_many :groups, serializer: GroupSerializer
+  #has_many :memberships, serializer: UserMembershipsSerializer
 end
