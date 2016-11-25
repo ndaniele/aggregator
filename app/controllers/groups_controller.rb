59792c50f0d4370cf512d94fade19f6855ddf55f
ndaniele <n.daniele@gmail.com>
2016-11-25 16:42:31 -0500

    added create, update, destroy and strong params to group_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,9 +11,30 @@ class GroupsController < ApplicationController
         render json: group
     end
     
+    def create    
+        group = Group.create(group_params)
+        #group.memberships.build(:user_id => params[:group][:user_id], :group_id => @group.id).save
+        render json: group #, status: 201
+    end
+    
+    def update
+        group = Group.find_by_id(params[:id])
+        group.update(group_params)
+        render json: group
+    end
+    
     def show
         group = Group.find_by_id(params[:id])
         render json: group
     end
+    
+    def destroy
+    end
+    
+     private 
+
+  def group_params
+    params.require(:group).permit(:id, :groupname)
+  end
     
 end 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email 
   has_many :answers 
-  has_many :questions
+  has_many :asked_questions, serializer: QuestionSerializer
   has_many :groups, serializer: GroupSerializer
   #has_many :memberships, serializer: UserMembershipsSerializer
 end
