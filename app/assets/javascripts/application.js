7d811699c373d83954d10be24c4ad2c20632b9e4
ndaniele <n.daniele@gmail.com>
2016-11-26 12:53:39 -0500

    added devise to front end assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-ui-router
+//= require angular-devise
+//= require angular-rails-templates
+//= require bootstrap-sprockets
+//= require moment
 //= require_tree .
