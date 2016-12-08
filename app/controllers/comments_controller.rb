7a903e33513aeafab9eb33486a52baf35be1acd2
ndaniele <n.daniele@gmail.com>
2016-12-08 18:02:07 -0500

    initial refactor of comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,38 +1,12 @@
 require 'pry'
 class CommentsController < ApplicationController
 
-# def new
-#   @question =  Question.find_by(id: params[:question_id])
-#   if @question == nil
-#       redirect_to root_path
-#   else
-#     #flash[:error] = "please select a question to answer"
-#     redirect_to question_path(@question)
-#   end
-# end
-
-# def show
-#   @question = Question.find_by(id: params[:question_id])
-#   @answers = @question.answers.build
-
-#   @question_answer = Question.find_by(:id => @question.id).answers.first
-#   respond_to do |f|
-#     f.html { render :show }
-#     f.json { render json: @question_answer }
-#   end
-# end
-
   def create
-      #binding.pry
     question =  Question.find_by(id: params[:question_id])
     comment = question.comments.build(comment: params["comment"], question_id: question.id)
-    #comment.user_id = params["user_id"] #session[:user_id]
     if
       comment.save
       render json: question
-    #else
-    #  flash[:error] = "!!You can only answer a question once!!"
-    #  redirect_to question_path(@question)
     end
   end
 
