4c7c65708b2614e63fb00af9c31645e11172abc2
ndaniele <n.daniele@gmail.com>
2016-12-08 18:04:27 -0500

    initial refactor of answers_controller.rb

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,38 +1,34 @@
 require 'pry'
 class AnswersController < ApplicationController
 
-  def new
-    @question =  Question.find_by(id: params[:question_id])
-    if @question == nil
-        redirect_to root_path
-    else
-      #flash[:error] = "please select a question to answer"
-      redirect_to question_path(@question)
-    end
-  end
+#  def new
+#    @question =  Question.find_by(id: params[:question_id])
+#    if @question == nil
+#        redirect_to root_path
+#    else
+#      #flash[:error] = "please select a question to answer"
+#      redirect_to question_path(@question)
+#    end
+#  end
 
-  def show
-    @question = Question.find_by(id: params[:question_id])
-    @answers = @question.answers.build
-
-    @question_answer = Question.find_by(:id => @question.id).answers.first
-    respond_to do |f|
-      f.html { render :show }
-      f.json { render json: @question_answer }
-    end
-  end
+#  def show
+#    @question = Question.find_by(id: params[:question_id])
+#    @answers = @question.answers.build
+#
+#    @question_answer = Question.find_by(:id => @question.id).answers.first
+#    respond_to do |f|
+#      f.html { render :show }
+#      f.json { render json: @question_answer }
+#    end
+#  end
 
   def create
-      #binding.pry
     question =  Question.find_by(id: params[:question_id])
     answer = question.answers.build(answer_params)
-    answer.user_id = current_user.id #session[:user_id]
+    answer.user_id = current_user.id
     if
       answer.save
       render json: question
-    #else
-    #  flash[:error] = "!!You can only answer a question once!!"
-    #  redirect_to question_path(@question)
     end
   end
 
