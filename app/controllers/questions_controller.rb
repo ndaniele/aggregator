ff3d4fcb75ab7992e972b12a4fd2bdfcc4f2d16b
ndaniele <n.daniele@gmail.com>
2016-12-08 17:58:34 -0500

    initial refactor of questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,6 @@ class QuestionsController < ApplicationController
     def question_data
         question = Question.find(params[:id])
         render json: QuestionSerializer.serialize(question)
-        #render question.to_json
     end
 
     def index
@@ -14,18 +13,16 @@ class QuestionsController < ApplicationController
         render json: questions
     end
 
-    def new
-        #raise params.inspect
-        @group = Group.find(params[:group])
-        @question = Question.new 
-        @question.answers.build
-    end
+ #   def new
+ #       #raise params.inspect
+ #       @group = Group.find(params[:group])
+ #       @question = Question.new 
+ #       @question.answers.build
+ #   end
     
     
     def my_questions
-        #binding.pry
         user = current_user
-        #user = User.find_by(id: 1)
         myQuestions = []
         userQuestions = user.groups.each do |group|
             group.group_questions.each do |groupQuestion|
@@ -37,9 +34,7 @@ class QuestionsController < ApplicationController
     
     
     def my_asked_questions
-        #binding.pry
         user = current_user
-        #current_user = User.find_by(id: 1)
         myAskedQuestions = []
         userAskedQuestions = 
             user.questions.each do |question|
@@ -49,66 +44,42 @@ class QuestionsController < ApplicationController
     end
     
     
-    #def create 
-        #raise params.inspect
-        #question = Question.create(question_params)
-        #render json: question
-    #end
-
-    
     def create
-       #raise params.inspect
-        #user = User.find_by_id(params[:user_id])
         user = current_user
-       question = user.asked_questions.build(question_params)
-       #group = Group.find_by(:id => params[:group_id])
-        if 
+        question = user.asked_questions.build(question_params)
+            if 
             question.save
                 groupArray = params["groups"]
                 groupArray.each { |id| 
-                    
-                #binding.pry
                     groupid = id 
-                    #group = Group.find_by(id: groupid)
                     GroupQuestion.create(group_id: groupid, question_id: question.id )
                 }
-            
-            
-            #group_question = GroupQuestion.create(:group_id => group.id, :question_id => question.id)
+ 
             render json: question
-  #          #redirect_to questions_path 
-  #          render json: @question, status: 201
-  #      else
-  #          flash[:error] = "#{@question.errors.full_messages.join(" & ")}"
-  #          redirect_to new_question_path(:group => @group.id)
         end
     end
 
-    def update
-        @question = Question.find(params[:id])
-        @question.update(question_params)
-        redirect_to question_answer_path(@question)
-    end
+ #   def update
+ #       @question = Question.find(params[:id])
+ #       @question.update(question_params)
+ #       redirect_to question_answer_path(@question)
+ #   end
 
     def show
         question = Question.find(params[:id])
         render json: question
     end
 
-    def destroy
-        @question = Question.find_by(id: params[:id])
-        @answers = Answer.where(question_id: @question.id)
-        @question.destroy
-
-        @answers.each do |answer|
-            answer.destroy
-        end
-        redirect_to user_path(current_user)
-        #redirect_to questions_path
-    end
-
-    def most_popular
-    end
+ #   def destroy
+ #       @question = Question.find_by(id: params[:id])
+ #       @answers = Answer.where(question_id: @question.id)
+ #       @question.destroy
+#
+ #       @answers.each do |answer|
+ #           answer.destroy
+ #       end
+ #       redirect_to user_path(current_user)
+ #   end
 
 
     private 
