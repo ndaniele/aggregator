ea6cb861bce37028e75b6846b7d910146396fe65
ndaniele <n.daniele@gmail.com>
2016-12-13 13:00:23 -0500

    refactor of groups.controller.js

--- app/assets/javascripts/groups/groups.controller.js
+++ app/assets/javascripts/groups/groups.controller.js
@@ -4,19 +4,7 @@ angular
 
 //CONTROLLER
     .controller('groupController', ['$scope', '$rootScope', 'GroupService', function($scope, $rootScope, GroupService) {
-        
-//        $scope.groupFilter = function(arr) {
-//            return $scope.myGroups.filter(check)
-//            
-//            function check(group) {
-//                for  (var i = 0; i < arr.length) {
-//                    return group.groupname === arr[i].groupname
-//                } 
-//                    
-//            
-//        };
-        
-        
+           
         $rootScope.checkedGroups = [];
         
         $scope.myGroups = '';
@@ -25,7 +13,6 @@ angular
             .getMyGroups()
             .then(function(response) {
                 return $scope.myGroups = response.data;
-//                groupFilter(['first group'])
         });
         
           $scope.updateMyGroups = function() {
@@ -33,24 +20,10 @@ angular
                 .getMyGroups()
                 .then(function(response) {
                 return $scope.myGroups = response.data;
-//                groupFilter(['first group'])
             });
         };
         
-   //    $scope.$watch('myGroups', function(newValue, oldValue) {
-       //    console.info('changed!');
-       //    console.log('Old:' + oldValue);
-       //    console.log('New:' + newValue);
-            //$apply()
-        //alert('hey, myGroups has changed!');
-            //console.log($scope.myGroups);
-  //  });
-        
-        //$scope.$watch('$scope.myGroups', function(newVal, oldVal) {
-        //    $scope.myGroups = $scope.myGroups.push(newVal);
-            
-        //});
-        
+
         $scope.groups = '';
         
         GroupService
@@ -76,12 +49,8 @@ angular
                 .createGroupMembership(groupid)
                 .then(function(response) {
                     alert('Group joined!');
-                //$scope.$apply(function(){$scope.myGroups = $scope.myGroups.push(response.data)});
                 return $scope.myGroups.push(response.data);  
             });
-            //$scope.$digest();
-            //$scope.$apply($scope.myGroups);
-            //$scope.$apply(function(){$scope.myGroups});
             console.log($scope.myGroups);
         };
 
@@ -91,13 +60,7 @@ angular
 
         $scope.checkClick = function(groupId) {
             $rootScope.checkedGroups.push(groupId);
-            //console.log($scope.checkedGroups);
-            //console.log(groupName + " box was checked");
         };
-
-        //$scope.joinClick = function(groupName) {
-           // console.log(groupName + " was joined");
-        //};
 
         $scope.newGroupClick = function(groupName) {
             console.log(groupName + " was created");
